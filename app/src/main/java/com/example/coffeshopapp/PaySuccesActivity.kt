32b44549edd8fcf2b0936252353e3ece46c25763
arekNgoding <rasyasastra18@gmail.com>
2023-10-24 00:05:39 -0700

    bagus

---
+++ app/src/main/java/com/example/coffeshopapp/PaySuccesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.coffeshopapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PaySuccesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pay_succes)
+    }
+}
